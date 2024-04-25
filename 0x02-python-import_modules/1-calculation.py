98867ba43c295b3cf2c41f9d0dc3186b26ef512e
Khadijun <muhachej@gmail.com>
2024-04-25 09:21:49 -0700

    Task 1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+add(a, b)
+sub(a, b)
+mul(a, b)
+div(a, b)
+
+print('{} + {} = {}'.format(10, 5, add(a, b)))
+print('{} - {} = {}'.format(10, 5, sub(a, b)))
+print('{} * {} = {}'.format(10, 5, mul(a, b)))
+print('{} / {} = {}'.format(10, 5, div(a, b)))
+
