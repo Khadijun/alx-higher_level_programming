9f409dae2511acac18dd88c62affa35a9c9ed5bf
Khadijun <muhachej@gmail.com>
2024-04-25 03:10:08 -0700

    Directories

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if idx < 0 or > len(my_list):
+        list[idx] = element
+        return list
---
+++ README.md
@@ -0,0 +1,5 @@
+HIGHER LEVEL LERNING PROGRAM
+
+A.PYTHON
+  *0x00. python - Hello, world.
+  *
