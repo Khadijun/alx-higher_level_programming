023c0798449366890669600683378fa8709d35a0
Khadijun <muhachej@gmail.com>
2024-04-27 01:12:33 -0700

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) - 1 == 0:
+        print('{:d} {}.'.format(0, 'arguments'))
+    else:
+        print('{:d} {}:'.format(len(sys.argv) - 1, 'arguments'))
+    for i in range(len(sys.argv) -1):
+        arg = sys.argv[i + 1]
+        print('{:d}: {}'.format(i + 1, arg))
