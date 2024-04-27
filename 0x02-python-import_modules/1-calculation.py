77db853f3dc4f50e237ec16cd14517f5a3cbf1c0
Khadijun <muhachej@gmail.com>
2024-04-26 23:36:58 -0700

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,12 +4,7 @@ if __name__ == '__main__':
     a = 10
     b = 5
 
-    add(a, b)
-    sub(a, b)
-    mul(a, b)
-    div(a, b)
-
-    print('{} + {} = {}'.format(10, 5, add(a, b)))
-    print('{} - {} = {}'.format(10, 5, sub(a, b)))
-    print('{} * {} = {}'.format(10, 5, mul(a, b)))
-    print('{} / {} = {}'.format(10, 5, div(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
