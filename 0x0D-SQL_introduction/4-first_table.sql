e73bd59b74b2b7b396b74b313891ed3d53c18a42
Khadijun <muhachej@gmail.com>
2024-05-22 08:34:36 -0700

    create table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,7 @@
 -- create a table called first_table in mysql
 -- The database name will be passed as an argument of the mysql command
 -- If the table first_table already exists, its ignored
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
 USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table(
