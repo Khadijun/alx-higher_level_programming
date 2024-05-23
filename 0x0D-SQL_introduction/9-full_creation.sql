c3b2aab6858bfcc190217feaf9a4290406c226dd
Khadijun <muhachej@gmail.com>
2024-05-23 02:19:29 -0700

    Creating and inserting values in a table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- create a new table
+-- insert rows into the tale and give the ttributes values
+
+CREATE TABLE IF NOT EXISTS second_table(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256),
+    score INT
+);
+
+INSERT INTO second_table (name, score) VALUES ( 'John', 10);
+INSERT INTO second_table (name, score) VALUES (  'Alex', 3 );
+INSERT INTO second_table (name, score) VALUES ( 'Bob', 14);
+INSERT INTO second_table (name, score) VALUES ( 'George', 8);
